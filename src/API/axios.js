275b56113b8e52e7698df13a28599d43cbc08504
Moges <mogesse741@gmial.com>
2025-12-22 23:23:56 +0300

    add backendUrl from vercel

--- src/API/axios.js
+++ src/API/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL : "http://127.0.0.1:5001/clone-3f347/us-central1/api",
+  // baseURL : "http://127.0.0.1:5001/clone-3f347/us-central1/api",  // Local URL of firebase function
+  baseURL : "https://api-project-r4pk.vercel.app/",  // Deployed URL of vercel function
 }) 
 
 export {axiosInstance}
