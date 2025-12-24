49d9289060cd718d9d84bc626cb6a5338541b8b3
Moges <mogesse741@gmial.com>
2025-12-24 09:17:58 +0300

    added backend URL from Render

--- src/API/axios.js
+++ src/API/axios.js
@@ -2,7 +2,19 @@ import axios from 'axios'
 
 const axiosInstance = axios.create({
   // baseURL : "http://127.0.0.1:5001/clone-3f347/us-central1/api",  // Local URL of firebase function
-  baseURL : "https://api-project-r4pk.vercel.app/",  // Deployed URL of vercel function
+  // baseURL : "https://api-project-r4pk.vercel.app/",  // Deployed URL of vercel function
+   baseURL: "https://amazon-backend-r36o.onrender.com" // using Render
 }) 
 
+
+
+
 export {axiosInstance}
+
+
+
+
+
+
+
+
